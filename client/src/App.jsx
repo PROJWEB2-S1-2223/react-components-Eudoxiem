eb9e7e6e9fcf151c3062bbe1f74935ae58110410
Eudoxiem <90182546+Eudoxiem@users.noreply.github.com>
2023-01-10 08:18:18 +0000

    second essai liste de courses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React from 'react';
 import './App.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 function App(){
   const box = <input type="checkbox"/>;
-  const article= [ { id:0,name:"poireaux",},
-                   {id:1,name:"oignons",},
-                   {id:2,name:"céléri",}
-                 ]
-
+  const articles = [{id:0,name:"poireaux"
+                  },
+                   {id:1,name:"oignons"
+                  },
+                   {id:2,name:"céléri"
+                  }];
   return (
-
       <div>
-        <h1>Ma liste de courses</h1>
+        <h1> Ma liste de courses. </h1>
         <ul>
-        const list=article.map((article)=> <li article key ={article.id}> {article.name}{box}) </li>
+            const List = articles.map ((item) => <li key={item.id}> {item.name} {box} </li>)
         </ul>
-       /*{<input type="checkbox"/>}*/
       </div>
   );
 }
-
+root.render(<App/>);
 export default App;
