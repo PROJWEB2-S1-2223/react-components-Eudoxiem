a9bd6105ce13143e32a01a29e8b4b8a715dfa220
Eudoxiem <90182546+Eudoxiem@users.noreply.github.com>
2023-01-15 19:45:52 +0000

    Ma liste de course avec chckbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,54 @@ import React from 'react';
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-function App(){
-  const box = <input type="checkbox"/>;
-  const articles = [{id:0,name:"poireaux"
-                  },
-                   {id:1,name:"oignons"
-                  },
-                   {id:2,name:"céléri"
-                  }];
+function App() {
+
   return (
+
       <div>
-        <h1> Ma liste de courses. </h1>
-        <ul>
-            const List = articles.map ((item) => <li key={item.id}> {item.name} {box} </li>)
-        </ul>
+         <Title/>
+         <List/>
       </div>
   );
 }
+
+function Title() {
+  return(
+    <div>
+      <h1> Ma liste de courses </h1>
+    </div>
+  );
+}
+
+function List(){
+  const box = <input type="checkbox"/>;
+  const articles=[
+    {id:0,name:'poireaux'},
+    {id:1,name:'céléri'},
+    {id:2,name:'persil'},
+    {id:3,name:'brocoli'},
+    {id:4,name:'laitue'},
+    {id:5,name:'cresson'},
+    {id:6,name:'ciboulette'},
+    {id:7,name:'basilic'},
+    {id:8,name:'estragon'},
+    {id:9,name:'aneth'}
+  ];
+  return (
+  <div>
+  <ul>
+    {articles.map(function displayItem(item){
+      return(<li key={item.id}>
+        <span>{item.name}</span>
+        <span> {box}</span>
+      </li>
+      );
+      })}
+  </ul>
+  </div>
+  );
+}
+
+
 root.render(<App/>);
 export default App;
