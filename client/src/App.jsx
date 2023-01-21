682911fd6e54d9fece406ba025fc0ee586504f51
Eudoxiem <90182546+Eudoxiem@users.noreply.github.com>
2023-01-21 16:42:40 +0000

    Retrait des functions Title et List et creation des fichiers List.jsx et Title.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,14 @@
 import React from 'react';
 import './App.css';
+import Title from './Title';
+import List from './List';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
-
   return (
-
-      <div>
-         <Title/>
-         <List/>
-      </div>
-  );
-}
-
-function Title() {
-  return(
     <div>
-      <h1> Ma liste de courses </h1>
+      <Title/>
+      <List/>
     </div>
   );
-}
-
-function List(){
-  const box = <input type="checkbox"/>;
-  const articles=[
-    {id:0,name:'poireaux'},
-    {id:1,name:'céléri'},
-    {id:2,name:'persil'},
-    {id:3,name:'brocoli'},
-    {id:4,name:'laitue'},
-    {id:5,name:'cresson'},
-    {id:6,name:'ciboulette'},
-    {id:7,name:'basilic'},
-    {id:8,name:'estragon'},
-    {id:9,name:'aneth'}
-  ];
-  return (
-  <div>
-  <ul>
-    {articles.map(function displayItem(item){
-      return(<li key={item.id}>
-        <span>{item.name}</span>
-        <span> {box}</span>
-      </li>
-      );
-      })}
-  </ul>
-  </div>
-  );
-}
-
-
-root.render(<App/>);
+  }
 export default App;
---
+++ client/src/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Title() {
+  return(
+    <div>
+      <h1> Ma liste de courses </h1>
+    </div>
+  );
+}
+export default Title;
